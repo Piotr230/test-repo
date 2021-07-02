41b2758aeb39ea4ac848d8fff89cefba98da8f71
Piotr_ <p.s230@interia.pl>
2021-07-02 11:21:18 +0200

    Zmiana Projekt 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Umiesc mnie na githubie");
+        System.out.println("Umiesc mnie na githubie i modyfikacja main");
     }
 }
